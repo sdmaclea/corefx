332e673f88562a4baf5a66d5d2d1377586803a70
Steve MacLean <Steve.MacLean@Microsoft.com>
2019-04-25 19:50:08 -0400

    ALC SatelliteAssemblies tests

---
+++ src/System.Runtime.Loader/tests/SatelliteAssemblies.cs
@@ -0,0 +1,110 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Globalization;
+using Xunit;
+using ReferencedClassLib;
+using ReferencedClassLibNeutralIsSatellite;
+
+namespace System.Runtime.Loader.Tests
+{
+    public class SatelliteAssembliesTests
+    {
+        Dictionary<string, AssemblyLoadContext> contexts = new Dictionary<string, AssemblyLoadContext>();
+        public SatelliteAssembliesTests()
+        {
+            AssemblyLoadContext satelliteAssembliesTests = new AssemblyLoadContext("SatelliteAssembliesTests");
+            satelliteAssembliesTests.LoadFromAssemblyPath(typeof(SatelliteAssembliesTests).Assembly.Location);
+
+            AssemblyLoadContext referencedClassLib = new AssemblyLoadContext("ReferencedClassLib");
+            referencedClassLib.LoadFromAssemblyPath(typeof(ReferencedClassLib.Program).Assembly.Location);
+
+            AssemblyLoadContext referencedClassLibNeutralIsSatellite = new AssemblyLoadContext("ReferencedClassLibNeutralIsSatellite");
+            referencedClassLibNeutralIsSatellite.LoadFromAssemblyPath(typeof(ReferencedClassLibNeutralIsSatellite.Program).Assembly.Location);
+            referencedClassLibNeutralIsSatellite.LoadFromAssemblyPath(typeof(ReferencedClassLibNeutralIsSatellite.Program).Assembly.Location);
+
+            new AssemblyLoadContext("Empty");
+
+            AssemblyLoadContext referencedClassLibEn = new AssemblyLoadContext("ReferencedClassLib.en");
+            string parentDir = Path.GetDirectoryName(typeof(ReferencedClassLib.Program).Assembly.Location);
+            string name = typeof(ReferencedClassLib.Program).Assembly.GetName().Name + ".resources.dll";
+            string path = Path.Combine(parentDir, "en", name);
+            Console.WriteLine(path);
+            // referencedClassLibEn.LoadFromAssemblyPath(Path.Combine(parentDir, "en", name));
+
+            Assembly assembly = Assembly.LoadFile(typeof(ReferencedClassLib.Program).Assembly.Location);
+            contexts["LoadFrom"] = AssemblyLoadContext.GetLoadContext(assembly);
+
+            foreach (var alc in AssemblyLoadContext.All)
+            {
+                if (alc.Name != null)
+                    contexts[alc.Name] = alc;
+            }
+        }
+
+        [Theory]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "",      "Neutral language Main description 1.0.0")]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "en",    "English language Main description 1.0.0")]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "en-US", "English language Main description 1.0.0")]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "es",    "Neutral language Main description 1.0.0")]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "es-MX", "Spanish (Mexico) language Main description 1.0.0")]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "fr",    "Neutral language Main description 1.0.0")]
+        [InlineData("Default", "System.Runtime.Loader.Tests.SatelliteAssembliesTests", "fr-FR", "Neutral language Main description 1.0.0")]
+        public void describeLib(string alc, string type, string culture, string expected)
+        {
+            string result = "Oops";
+            try
+            {
+                using (contexts[alc].EnterContextualReflection())
+                {
+                    Type describeType = Type.GetType(type);
+
+                    result = (String)describeType.InvokeMember("Describe", BindingFlags.InvokeMethod, null, null, new object[] { culture });
+                }
+
+            }
+            catch
+            {
+                result = "threw";
+            }
+
+            Assert.Equal(expected, result);
+        }
+        
+        public static string Describe(string lang)
+        {
+            ResourceManager rm = new ResourceManager(typeof(MainStrings));
+
+            CultureInfo ci = CultureInfo.CreateSpecificCulture(lang);
+            
+            return rm.GetString("Describe", ci);
+        }
+
+        [Theory]
+        [InlineData("", "Neutral language Main description 1.0.0")]
+        [InlineData("en", "English language Main description 1.0.0")]
+        [InlineData("en-US", "English language Main description 1.0.0")]
+        [InlineData("es", "Neutral language Main description 1.0.0")]
+        [InlineData("es-MX", "Spanish (Mexico) language Main description 1.0.0")]
+        [InlineData("fr", "Neutral language Main description 1.0.0")]
+        [InlineData("fr-FR", "Neutral language Main description 1.0.0")]
+        static public void mainResources(string lang, string expected)
+        {
+            ResourceManager rm = new ResourceManager(typeof(MainStrings));
+
+            CultureInfo ci = CultureInfo.CreateSpecificCulture(lang);
+
+            Assert.Equal(expected, Describe(lang));
+        }
+    }
+
+    class MainStrings
+    {
+    }
+}
+
